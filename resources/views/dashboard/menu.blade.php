ecc29e87da9d717409ca276fb52160e111d0d2e6
a.umnov@exahost.ru <a.umnov@exahost.ru>
2016-09-13 21:53:01 +0300

    Добавил тему, отрисовал login-страницу

---
+++ resources/views/dashboard/menu.blade.php
@@ -0,0 +1,57 @@
+@section('menu')
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <!--<li><a href="#"><i class="fa fa-user fa-fw"></i> Профиль</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Настройки</a>
+                        </li>
+                        <li class="divider"></li>
+						-->
+                        <li><a href="{{ url('/logout') }}"
+							onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+							<i class="fa fa-sign-out fa-fw"></i> Выход</a>
+							
+							<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="{{ url('/') }}"><i class="fa fa-dashboard fa-fw"></i> Панель управления</a>
+                        </li>
+                        <li>
+                            <a href="{{ url('/server-list') }}"><i class="fa fa-list fa-fw"></i> Список серверов</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//var_dump(Auth::routes());exit();
+#Auth::routes();
+#Route::get('/home', 'HomeController@index');
+
+# Роуты для входа/выхода
+Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('login', ['as' => 'auth.login', 'uses' => 'Auth\LoginController@login']);
+Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@logout']);
+Route::post('logout', ['as' => 'auth.logout', 'uses' => 'Auth\LoginController@logout']);
+#Роуты для регистрации
+// По какой-то причине редиректит на /home зарегистрированных пользователей, надо делать свой контроллер
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+	Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\RegisterController@register']);
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+	Route::get('/', 'Dashboard@index');
+	Route::get('/server-list', 'Dashboard@server_list');
+});
